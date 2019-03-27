24c1a63f2121685bdccb7bc081a17484a7709987
qbasa12 <48801407+qbasa12@users.noreply.github.com>
2019-03-27 20:18:12 +0000

    Update CImageLoaderPNG.h

--- src/irr/asset/CImageLoaderPNG.h
+++ src/irr/asset/CImageLoaderPNG.h
@@ -2,149 +2,297 @@
 // This file is part of the "Irrlicht Engine".
 // For conditions of distribution and use, see copyright notice in irrlicht.h
 
-#include "CImageWriterTGA.h"
+#include "CImageLoaderPNG.h"
+#include <iostream>   
+#include <string>  
+#include <string.h> 
 
-#ifdef _IRR_COMPILE_WITH_TGA_WRITER_
+#ifdef _IRR_COMPILE_WITH_PNG_LOADER_
 
-#include "CImageLoaderTGA.h"
-#include "IWriteFile.h"
-#include "CColorConverter.h"
+#ifdef _IRR_COMPILE_WITH_LIBPNG_
+	#ifndef _IRR_USE_NON_SYSTEM_LIB_PNG_
+	#include <png.h> // use system lib png
+	#else // _IRR_USE_NON_SYSTEM_LIB_PNG_
+	#include "libpng/png.h" // use irrlicht included lib png
+	#endif // _IRR_USE_NON_SYSTEM_LIB_PNG_
+#endif // _IRR_COMPILE_WITH_LIBPNG_
+
 #include "irr/asset/ICPUTexture.h"
+#include "irr/asset/CImageData.h"
+#include "CReadFile.h"
+#include "os.h"
+
 
 namespace irr
 {
 namespace asset
 {
 
-CImageWriterTGA::CImageWriterTGA()
+#ifdef _IRR_COMPILE_WITH_LIBPNG_
+// PNG function for error handling
+static void png_cpexcept_error(png_structp png_ptr, png_const_charp msg)
 {
-#ifdef _DEBUG
-	setDebugName("CImageWriterTGA");
-#endif
+	os::Printer::log("PNG fatal error", msg, ELL_ERROR);
+	longjmp(png_jmpbuf(png_ptr), 1);
 }
 
-bool CImageWriterTGA::writeAsset(io::IWriteFile* _file, const SAssetWriteParams& _params, IAssetWriterOverride* _override)
+// PNG function for warning handling
+static void png_cpexcept_warn(png_structp png_ptr, png_const_charp msg)
 {
-    if (!_override)
-        getDefaultOverride(_override);
+	os::Printer::log("PNG warning", msg, ELL_WARNING);
+}
 
-    SAssetWriteContext ctx{_params, _file};
+// PNG function for file reading
+void PNGAPI user_read_data_fcn(png_structp png_ptr, png_bytep data, png_size_t length)
+{
+	png_size_t check;
 
-    const asset::CImageData* image =
-#   ifndef _DEBUG
-        static_cast<const asset::CImageData*>(_params.rootAsset);
-#   else
-        dynamic_cast<const asset::CImageData*>(_params.rootAsset);
-#   endif
-    assert(image);
+	// changed by zola {
+	io::IReadFile* file=(io::IReadFile*)png_get_io_ptr(png_ptr);
+	check=(png_size_t) file->read((void*)data,(uint32_t)length);
+	// }
 
-    io::IWriteFile* file = _override->getOutputFile(_file, ctx, { image, 0u });
+	if (check != length)
+		png_error(png_ptr, "Read Error");
+}
+#endif // _IRR_COMPILE_WITH_LIBPNG_
 
-	STGAHeader imageHeader;
-	imageHeader.IdLength = 0;
-	imageHeader.ColorMapType = 0;
-	imageHeader.ImageType = 2;
-	imageHeader.FirstEntryIndex[0] = 0;
-	imageHeader.FirstEntryIndex[1] = 0;
-	imageHeader.ColorMapLength = 0;
-	imageHeader.ColorMapEntrySize = 0;
-	imageHeader.XOrigin[0] = 0;
-	imageHeader.XOrigin[1] = 0;
-	imageHeader.YOrigin[0] = 0;
-	imageHeader.YOrigin[1] = 0;
-	imageHeader.ImageWidth = image->getSize().X;
-	imageHeader.ImageHeight = image->getSize().Y;
 
-	// top left of image is the top. the image loader needs to
-	// be fixed to only swap/flip
-	imageHeader.ImageDescriptor = (1 << 5);
+//! returns true if the file maybe is able to be loaded by this class
+bool CImageLoaderPng::isALoadableFileFormat(io::IReadFile* _file) const
+{
+#ifdef _IRR_COMPILE_WITH_LIBPNG_
+	if (!_file)
+		return false;
 
-   // chances are good we'll need to swizzle data, so i'm going
-	// to convert and write one scan line at a time. it's also
-	// a bit cleaner this way
-	void (*CColorConverter_convertFORMATtoFORMAT)(const void*, int32_t, void*) = 0;
-	switch(image->getColorFormat())
+    const size_t prevPos = _file->getPos();
+
+	png_byte buffer[8];
+	// Read the first few bytes of the PNG _file
+    if (_file->read(buffer, 8) != 8)
+    {
+        _file->seek(prevPos);
+        return false;
+    }
+
+    _file->seek(prevPos);
+	// Check if it really is a PNG _file
+	return !png_sig_cmp(buffer, 0, 8);
+#else
+	return false;
+#endif // _IRR_COMPILE_WITH_LIBPNG_
+}
+
+
+// load in the image data
+asset::IAsset* CImageLoaderPng::loadAsset(io::IReadFile* _file, const asset::IAssetLoader::SAssetLoadParams& _params, asset::IAssetLoader::IAssetLoaderOverride* _override, uint32_t _hierarchyLevel)
+{
+    core::vector<asset::CImageData*> images;
+#ifdef _IRR_COMPILE_WITH_LIBPNG_
+	if (!_file)
+		return nullptr;
+
+	asset::CImageData* image = 0;
+	//Used to point to image rows
+	uint8_t** RowPointers = 0;
+
+	png_byte buffer[8];
+	// Read the first few bytes of the PNG _file
+	if( _file->read(buffer, 8) != 8 )
 	{
-	case asset::EF_B8G8R8A8_UNORM:
-		CColorConverter_convertFORMATtoFORMAT
-			= video::CColorConverter::convert_A8R8G8B8toA8R8G8B8;
-		imageHeader.PixelDepth = 32;
-		imageHeader.ImageDescriptor |= 8;
-		break;
-	case asset::EF_A1R5G5B5_UNORM_PACK16:
-		CColorConverter_convertFORMATtoFORMAT
-			= video::CColorConverter::convert_A1R5G5B5toA1R5G5B5;
-		imageHeader.PixelDepth = 16;
-		imageHeader.ImageDescriptor |= 1;
-		break;
-	case asset::EF_B5G6R5_UNORM_PACK16:
-		CColorConverter_convertFORMATtoFORMAT
-			= video::CColorConverter::convert_R5G6B5toA1R5G5B5;
-		imageHeader.PixelDepth = 16;
-		imageHeader.ImageDescriptor |= 1;
-		break;
-	case asset::EF_R8G8B8_UNORM:
-		CColorConverter_convertFORMATtoFORMAT
-			= video::CColorConverter::convert_R8G8B8toR8G8B8;
-		imageHeader.PixelDepth = 24;
-		imageHeader.ImageDescriptor |= 0;
-		break;
-#ifndef _DEBUG
-	default:
-		break;
-#endif
+		os::Printer::log("LOAD PNG: can't read _file\n", _file->getFileName().c_str(), ELL_ERROR);
+		return nullptr;
 	}
 
-	// couldn't find a color converter
-	if (!CColorConverter_convertFORMATtoFORMAT)
-		return false;
-
-	if (file->write(&imageHeader, sizeof(imageHeader)) != sizeof(imageHeader))
-		return false;
-
-	uint8_t* scan_lines = (uint8_t*)image->getData();
-	if (!scan_lines)
-		return false;
-
-	// size of one pixel in bits
-	uint32_t pixel_size_bits = image->getBitsPerPixel();
-
-	// length of one row of the source image in bytes
-	uint32_t row_stride = (pixel_size_bits * imageHeader.ImageWidth)/8;
-
-	// length of one output row in bytes
-	int32_t row_size = ((imageHeader.PixelDepth / 8) * imageHeader.ImageWidth);
-
-	// allocate a row do translate data into
-	uint8_t* row_pointer = new uint8_t[row_size];
-
-	uint32_t y;
-	for (y = 0; y < imageHeader.ImageHeight; ++y)
+	// Check if it really is a PNG _file
+	if( png_sig_cmp(buffer, 0, 8) )
 	{
-		// source, length [pixels], destination
-		if (image->getColorFormat()==asset::EF_R8G8B8_UNORM)
-            video::CColorConverter::convert24BitTo24Bit(&scan_lines[y * row_stride], row_pointer, imageHeader.ImageWidth, 1, 0, 0, true);
+		os::Printer::log("LOAD PNG: not really a png\n", _file->getFileName().c_str(), ELL_ERROR);
+		return nullptr;
+	}
+
+	// Allocate the png read struct
+	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+		NULL, (png_error_ptr)png_cpexcept_error, (png_error_ptr)png_cpexcept_warn);
+	if (!png_ptr)
+	{
+		os::Printer::log("LOAD PNG: Internal PNG create read struct failure\n", _file->getFileName().c_str(), ELL_ERROR);
+		return nullptr;
+	}
+
+	// Allocate the png info struct
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+	{
+		os::Printer::log("LOAD PNG: Internal PNG create info struct failure\n", _file->getFileName().c_str(), ELL_ERROR);
+		png_destroy_read_struct(&png_ptr, NULL, NULL);
+		return nullptr;
+	}
+
+	// for proper error handling
+	if (setjmp(png_jmpbuf(png_ptr)))
+	{
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		if (RowPointers)
+			delete [] RowPointers;
+		return nullptr;
+	}
+
+	// changed by zola so we don't need to have public FILE pointers
+	png_set_read_fn(png_ptr, _file, user_read_data_fcn);
+
+	png_set_sig_bytes(png_ptr, 8); // Tell png that we read the signature
+
+	png_read_info(png_ptr, info_ptr); // Read the info section of the png _file
+
+	uint32_t imageSize[3] = {1,1,1};
+	uint32_t& Width = imageSize[0];
+	uint32_t& Height = imageSize[1];
+	int32_t BitDepth;
+	int32_t ColorType;
+	{
+		// Use temporary variables to avoid passing casted pointers
+		png_uint_32 w,h;
+		// Extract info
+		png_get_IHDR(png_ptr, info_ptr,
+			&w, &h,
+			&BitDepth, &ColorType, NULL, NULL, NULL);
+		Width=w;
+		Height=h;
+	}
+
+	// Convert palette color to true color
+	if (ColorType==PNG_COLOR_TYPE_PALETTE)
+		png_set_palette_to_rgb(png_ptr);
+
+	// Convert low bit colors to 8 bit colors
+	if (BitDepth < 8)
+	{
+		if (ColorType==PNG_COLOR_TYPE_GRAY || ColorType==PNG_COLOR_TYPE_GRAY_ALPHA)
+			png_set_expand_gray_1_2_4_to_8(png_ptr);
 		else
-			CColorConverter_convertFORMATtoFORMAT(&scan_lines[y * row_stride], imageHeader.ImageWidth, row_pointer);
-		if (file->write(row_pointer, row_size) != row_size)
-			break;
+			png_set_packing(png_ptr);
 	}
 
-	delete [] row_pointer;
+	if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+		png_set_tRNS_to_alpha(png_ptr);
 
-	STGAFooter imageFooter;
-	imageFooter.ExtensionOffset = 0;
-	imageFooter.DeveloperOffset = 0;
-	strncpy(imageFooter.Signature, "TRUEVISION-XFILE.", 18);
+	// Convert high bit colors to 8 bit colors
+	if (BitDepth == 16)
+		png_set_strip_16(png_ptr);
 
-	if (file->write(&imageFooter, sizeof(imageFooter)) < (int32_t)sizeof(imageFooter))
-		return false;
+	// Convert gray color to true color
+	if (ColorType==PNG_COLOR_TYPE_GRAY || ColorType==PNG_COLOR_TYPE_GRAY_ALPHA)
+		png_set_gray_to_rgb(png_ptr);
 
-	return imageHeader.ImageHeight <= y;
+	int intent;
+	const double screen_gamma = 2.2;
+
+	if (png_get_sRGB(png_ptr, info_ptr, &intent))
+		png_set_gamma(png_ptr, screen_gamma, 0.45455);
+	else
+	{
+		double image_gamma;
+
+		if (png_get_gAMA(png_ptr, info_ptr, &image_gamma)) {
+
+		
+			png_set_gamma(png_ptr, screen_gamma, image_gamma);
+		}
+
+		else {
+			
+			os::Printer::log(std::to_wstring(image_gamma), ELL_DEBUG);
+	
+			png_set_gamma(png_ptr, screen_gamma, 0.45455);
+		}
+	}
+
+	// Update the changes in between, as we need to get the new color type
+	// for proper processing of the RGBA type
+	png_read_update_info(png_ptr, info_ptr);
+	{
+		// Use temporary variables to avoid passing casted pointers
+		png_uint_32 w,h;
+		// Extract info
+		png_get_IHDR(png_ptr, info_ptr,
+			&w, &h,
+			&BitDepth, &ColorType, NULL, NULL, NULL);
+		Width=w;
+		Height=h;
+	}
+
+	// Convert RGBA to BGRA
+	if (ColorType==PNG_COLOR_TYPE_RGB_ALPHA)
+	{
+		png_set_bgr(png_ptr);
+	}
+
+	// Create the image structure to be filled by png data
+	uint32_t nullOffset[3] = {0,0,0};
+	if (ColorType==PNG_COLOR_TYPE_RGB_ALPHA)
+		image = new asset::CImageData(NULL, nullOffset, imageSize, 0, asset::EF_B8G8R8A8_UNORM);
+	else
+		image = new asset::CImageData(NULL, nullOffset, imageSize, 0, asset::EF_R8G8B8_UNORM);
+	if (!image)
+	{
+		os::Printer::log("LOAD PNG: Internal PNG create image struct failure\n", _file->getFileName().c_str(), ELL_ERROR);
+		png_destroy_read_struct(&png_ptr, NULL, NULL);
+		return nullptr;
+	}
+
+	// Create array of pointers to rows in image data
+	RowPointers = new png_bytep[Height];
+	if (!RowPointers)
+	{
+		os::Printer::log("LOAD PNG: Internal PNG create row pointers failure\n", _file->getFileName().c_str(), ELL_ERROR);
+		png_destroy_read_struct(&png_ptr, NULL, NULL);
+		image->drop();
+		return nullptr;
+	}
+
+	// Fill array of pointers to rows in image data
+	uint8_t* data = reinterpret_cast<uint8_t*>(image->getData());
+	;
+	for (uint32_t i=0; i<Height; ++i)
+	{
+		RowPointers[i]=data;
+		
+		os::Printer::log(std::to_wstring(*data), ELL_DEBUG);
+			
+		data += image->getPitchIncludingAlignment();
+	}
+
+	// for proper error handling
+	if (setjmp(png_jmpbuf(png_ptr)))
+	{
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		delete [] RowPointers;
+		image->drop();
+		return nullptr;
+	}
+
+	// Read data using the library function that handles all transformations including interlacing
+	png_read_image(png_ptr, RowPointers);
+
+	png_read_end(png_ptr, NULL);
+	delete [] RowPointers;
+	png_destroy_read_struct(&png_ptr,&info_ptr, 0); // Clean up memory
+
+	images.push_back(image);
+#endif // _IRR_COMPILE_WITH_LIBPNG_
+
+    asset::ICPUTexture* tex = asset::ICPUTexture::create(images);
+    for (auto img : images)
+        img->drop();
+    return tex;
 }
 
-} // namespace video
-} // namespace irr
+
+}// end namespace irr
+}//end namespace video
 
 #endif
+
+
 
