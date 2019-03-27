a5d7402dd90cd8d7053bb2f282066b6a96bbb55f
qbasa12 <48801407+qbasa12@users.noreply.github.com>
2019-03-27 19:27:31 +0000

    Update CImageLoaderJPG.cpp

--- src/irr/asset/CImageLoaderJPG.cpp
+++ src/irr/asset/CImageLoaderJPG.cpp
@@ -2,293 +2,220 @@
 // This file is part of the "Irrlicht Engine".
 // For conditions of distribution and use, see copyright notice in irrlicht.h
 
-#include "CImageLoaderJPG.h"
+#include "CImageWriterJPG.h"
 
-#ifdef _IRR_COMPILE_WITH_JPG_LOADER_
+#ifdef _IRR_COMPILE_WITH_JPG_WRITER_
 
-#include "IReadFile.h"
+#include "CColorConverter.h"
+#include "IWriteFile.h"
 #include "CImage.h"
-#include "os.h"
 #include "irr/asset/ICPUTexture.h"
-#include <string>
-
-namespace irr
-{
-namespace asset
-{
-
-//! constructor
-CImageLoaderJPG::CImageLoaderJPG()
-{
-	#ifdef _DEBUG
-	setDebugName("CImageLoaderJPG");
-	#endif
-}
-
-
-
-//! destructor
-CImageLoaderJPG::~CImageLoaderJPG()
-{
-}
-
 
 #ifdef _IRR_COMPILE_WITH_LIBJPEG_
-
-    // struct for handling jpeg errors
-    struct irr_jpeg_error_mgr
-    {
-        // public jpeg error fields
-        struct jpeg_error_mgr pub;
-
-        // for longjmp, to return to caller on a fatal error
-        jmp_buf setjmp_buffer;
-    };
-
-void CImageLoaderJPG::init_source (j_decompress_ptr cinfo)
+#include <stdio.h> // required for jpeglib.h
+extern "C"
 {
-	// DO NOTHING
+#ifndef _IRR_USE_NON_SYSTEM_JPEG_LIB_
+	#include <jpeglib.h>
+	#include <jerror.h>
+#else
+	#include "jpeglib/jpeglib.h"
+	#include "jpeglib/jerror.h"
+#endif
+}
+
+// The writer uses a 4k buffer and flushes to disk each time it's filled
+#define OUTPUT_BUF_SIZE 4096
+
+using namespace irr;
+using namespace asset;
+
+namespace 
+{
+typedef struct
+{
+	struct jpeg_destination_mgr pub;/* public fields */
+
+	io::IWriteFile* file;		/* target file */
+	JOCTET buffer[OUTPUT_BUF_SIZE];	/* image buffer */
+} mem_destination_mgr;
+
+
+typedef mem_destination_mgr * mem_dest_ptr;
+}
+
+// init
+static void jpeg_init_destination(j_compress_ptr cinfo)
+{
+	mem_dest_ptr dest = (mem_dest_ptr) cinfo->dest;
+	dest->pub.next_output_byte = dest->buffer;
+	dest->pub.free_in_buffer = OUTPUT_BUF_SIZE;
 }
 
 
-
-boolean CImageLoaderJPG::fill_input_buffer (j_decompress_ptr cinfo)
+// flush to disk and reset buffer
+static boolean jpeg_empty_output_buffer(j_compress_ptr cinfo)
 {
-	// DO NOTHING
-	return 1;
+	mem_dest_ptr dest = (mem_dest_ptr) cinfo->dest;
+
+	// for now just exit upon file error
+	if (dest->file->write(dest->buffer, OUTPUT_BUF_SIZE) != OUTPUT_BUF_SIZE)
+		ERREXIT (cinfo, JERR_FILE_WRITE);
+
+	dest->pub.next_output_byte = dest->buffer;
+	dest->pub.free_in_buffer = OUTPUT_BUF_SIZE;
+
+	return TRUE;
 }
 
 
-
-void CImageLoaderJPG::skip_input_data (j_decompress_ptr cinfo, long count)
+static void jpeg_term_destination(j_compress_ptr cinfo)
 {
-	jpeg_source_mgr * src = cinfo->src;
-	if(count > 0)
-	{
-		src->bytes_in_buffer -= count;
-		src->next_input_byte += count;
+	mem_dest_ptr dest = (mem_dest_ptr) cinfo->dest;
+	const int32_t datacount = (int32_t)(OUTPUT_BUF_SIZE - dest->pub.free_in_buffer);
+	// for now just exit upon file error
+	if (dest->file->write(dest->buffer, datacount) != datacount)
+		ERREXIT (cinfo, JERR_FILE_WRITE);
+}
+
+
+// set up buffer data
+static void jpeg_file_dest(j_compress_ptr cinfo, io::IWriteFile* file)
+{
+	if (cinfo->dest == NULL)
+	{ /* first time for this JPEG object? */
+		cinfo->dest = (struct jpeg_destination_mgr *)
+			(*cinfo->mem->alloc_small) ((j_common_ptr) cinfo,
+						JPOOL_PERMANENT,
+						sizeof(mem_destination_mgr));
 	}
+
+	mem_dest_ptr dest = (mem_dest_ptr) cinfo->dest;  /* for casting */
+
+	/* Initialize method pointers */
+	dest->pub.init_destination = jpeg_init_destination;
+	dest->pub.empty_output_buffer = jpeg_empty_output_buffer;
+	dest->pub.term_destination = jpeg_term_destination;
+
+	/* Initialize private member */
+	dest->file = file;
 }
 
 
-
-void CImageLoaderJPG::term_source (j_decompress_ptr cinfo)
+/* write_JPEG_memory: store JPEG compressed image into memory.
+*/
+static bool writeJPEGFile(io::IWriteFile* file, const asset::CImageData* image, uint32_t quality)
 {
-	// DO NOTHING
-}
+	void (*format)(const void*, int32_t, void*) = 0;
+	switch( image->getColorFormat () )
+	{
+		case asset::EF_R8G8B8_UNORM:
+			format = video::CColorConverter::convert_R8G8B8toR8G8B8;
+			break;
+		case asset::EF_B8G8R8A8_UNORM:
+			format = video::CColorConverter::convert_A8R8G8B8toR8G8B8;
+			break;
+		case asset::EF_A1R5G5B5_UNORM_PACK16:
+			format = video::CColorConverter::convert_A1R5G5B5toB8G8R8;
+			break;
+		case asset::EF_B5G6R5_UNORM_PACK16:
+			format = video::CColorConverter::convert_R5G6B5toR8G8B8;
+			break;
+#ifndef _DEBUG
+		default:
+			break;
+#endif
+	}
 
-
-void CImageLoaderJPG::error_exit (j_common_ptr cinfo)
-{
-	// unfortunately we need to use a goto rather than throwing an exception
-	// as gcc crashes under linux crashes when using throw from within
-	// extern c code
-
-	// Always display the message
-	(*cinfo->err->output_message) (cinfo);
-
-	// cinfo->err really points to a irr_error_mgr struct
-	irr_jpeg_error_mgr *myerr = (irr_jpeg_error_mgr*) cinfo->err;
-
-	longjmp(myerr->setjmp_buffer, 1);
-}
-
-
-void CImageLoaderJPG::output_message(j_common_ptr cinfo)
-{
-	// display the error message.
-	char temp1[JMSG_LENGTH_MAX];
-	(*cinfo->err->format_message)(cinfo, temp1);
-	std::string errMsg("JPEG FATAL ERROR in ");
-    errMsg += reinterpret_cast<char*>(cinfo->client_data);
-	os::Printer::log(errMsg,temp1, ELL_ERROR);
-}
-#endif // _IRR_COMPILE_WITH_LIBJPEG_
-
-//! returns true if the file maybe is able to be loaded by this class
-bool CImageLoaderJPG::isALoadableFileFormat(io::IReadFile* _file) const
-{
-	#ifndef _IRR_COMPILE_WITH_LIBJPEG_
-	return false;
-	#else
-
-	if (!_file)
+	// couldn't find a color converter
+	if ( 0 == format )
 		return false;
 
-    const size_t prevPos = _file->getPos();
+	const core::vector3d<uint32_t> dim = image->getSize();
 
-	int32_t jfif = 0;
-	_file->seek(6);
-	_file->read(&jfif, sizeof(int32_t));
-    _file->seek(prevPos);
-	return (jfif == 0x4a464946 || jfif == 0x4649464a);
+	struct jpeg_compress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	cinfo.err = jpeg_std_error(&jerr);
 
-	#endif
-}
+	jpeg_create_compress(&cinfo);
+	jpeg_file_dest(&cinfo, file);
+	cinfo.image_width = dim.X;
+	cinfo.image_height = dim.Y;
+	cinfo.input_components = 3;
+	cinfo.in_color_space = JCS_RGB;
 
-//! creates a surface from the file
-asset::IAsset* CImageLoaderJPG::loadAsset(io::IReadFile* _file, const asset::IAssetLoader::SAssetLoadParams& _params, asset::IAssetLoader::IAssetLoaderOverride* _override, uint32_t _hierarchyLevel)
-{
-    core::vector<asset::CImageData*> images;
+	jpeg_set_defaults(&cinfo);
 
-#ifndef _IRR_COMPILE_WITH_LIBJPEG_
-	os::Printer::log("Can't load as not compiled with _IRR_COMPILE_WITH_LIBJPEG_:", _file->getFileName(), ELL_DEBUG);
-#else
-	if (!_file)
-		return nullptr;
+	if ( 0 == quality )
+		quality = 75;
 
-	const io::path& Filename = _file->getFileName();
+	jpeg_set_quality(&cinfo, quality, TRUE);
+	jpeg_start_compress(&cinfo, TRUE);
 
-	uint8_t **rowPtr=0;
-	uint8_t* input = new uint8_t[_file->getSize()];
-	_file->read(input, _file->getSize());
+	uint8_t * dest = new uint8_t[dim.X*3];
 
-	// allocate and initialize JPEG decompression object
-	struct jpeg_decompress_struct cinfo;
-	struct irr_jpeg_error_mgr jerr;
-
-	//We have to set up the error handler first, in case the initialization
-	//step fails.  (Unlikely, but it could happen if you are out of memory.)
-	//This routine fills in the contents of struct jerr, and returns jerr's
-	//address which we place into the link field in cinfo.
-
-	cinfo.err = jpeg_std_error(&jerr.pub);
-	cinfo.err->error_exit = error_exit;
-	cinfo.err->output_message = output_message;
-    cinfo.client_data = const_cast<char*>(Filename.c_str());
-
-	// compatibility fudge:
-	// we need to use setjmp/longjmp for error handling as gcc-linux
-	// crashes when throwing within external c code
-	if (setjmp(jerr.setjmp_buffer))
+	if (dest)
 	{
-		// If we get here, the JPEG code has signaled an error.
-		// We need to clean up the JPEG object and return.
+		const uint32_t pitch = image->getPitch();
+		JSAMPROW row_pointer[1];      /* pointer to JSAMPLE row[s] */
+		row_pointer[0] = dest;
 
-		jpeg_destroy_decompress(&cinfo);
+		uint8_t* src = (uint8_t*)image->getData();
 
-		delete [] input;
-		// if the row pointer was created, we delete it.
-		if (rowPtr)
-			delete [] rowPtr;
-
-		// return null pointer
-		return nullptr;
-	}
-
-	// Now we can initialize the JPEG decompression object.
-	jpeg_create_decompress(&cinfo);
-
-	// specify data source
-	jpeg_source_mgr jsrc;
-
-	// Set up data pointer
-	jsrc.bytes_in_buffer = _file->getSize();
-	jsrc.next_input_byte = (JOCTET*)input;
-	cinfo.src = &jsrc;
-
-	jsrc.init_source = init_source;
-	jsrc.fill_input_buffer = fill_input_buffer;
-	jsrc.skip_input_data = skip_input_data;
-	jsrc.resync_to_restart = jpeg_resync_to_restart;
-	jsrc.term_source = term_source;
-
-	// Decodes JPG input from whatever source
-	// Does everything AFTER jpeg_create_decompress
-	// and BEFORE jpeg_destroy_decompress
-	// Caller is responsible for arranging these + setting up cinfo
-
-	// read _file parameters with jpeg_read_header()
-	jpeg_read_header(&cinfo, TRUE);
-
-	bool useCMYK=false;
-	if (cinfo.jpeg_color_space==JCS_CMYK)
-	{
-		cinfo.out_color_space=JCS_CMYK;
-		cinfo.out_color_components=4;
-		useCMYK=true;
-	}
-	else
-	{
-		cinfo.out_color_space=JCS_RGB;
-		cinfo.out_color_components=3;
-	}
-	cinfo.output_gamma=2.2;
-	cinfo.do_fancy_upsampling=FALSE;
-
-	// Start decompressor
-	jpeg_start_decompress(&cinfo);
-
-	// Get image data
-	uint16_t rowspan = cinfo.image_width * cinfo.out_color_components;
-	uint32_t imageSize[3] = {cinfo.image_width,cinfo.image_height,1};
-	uint32_t& width = imageSize[0];
-	uint32_t& height = imageSize[1];
-
-	// Allocate memory for buffer
-	uint8_t* output = reinterpret_cast<uint8_t*>(_IRR_ALIGNED_MALLOC(rowspan * height,_IRR_SIMD_ALIGNMENT));
-
-	// Here we use the library's state variable cinfo.output_scanline as the
-	// loop counter, so that we don't have to keep track ourselves.
-	// Create array of row pointers for lib
-	rowPtr = new uint8_t* [height];
-
-	for( uint32_t i = 0; i < height; i++ )
-		rowPtr[i] = &output[ i * rowspan ];
-
-	uint32_t rowsRead = 0;
-
-	while( cinfo.output_scanline < cinfo.output_height )
-		rowsRead += jpeg_read_scanlines( &cinfo, &rowPtr[rowsRead], cinfo.output_height - rowsRead );
-
-	delete [] rowPtr;
-	// Finish decompression
-
-	jpeg_finish_decompress(&cinfo);
-
-	// Release JPEG decompression object
-	// This is an important step since it will release a good deal of memory.
-	jpeg_destroy_decompress(&cinfo);
-
-	uint32_t nullOffset[3] = {0,0,0};
-	// convert image
-    asset::CImageData* image = 0;
-	if (useCMYK)
-	{
-		image = new asset::CImageData(NULL,nullOffset,imageSize,0,asset::EF_R8G8B8_UNORM);
-		const uint32_t size = 3*width*height;
-		uint8_t* data = (uint8_t*)image->getData();
-		if (data)
+		while (cinfo.next_scanline < cinfo.image_height)
 		{
-			for (uint32_t i=0,j=0; i<size; i+=3, j+=4)
-			{
-				// Also works without K, but has more contrast with K multiplied in
-//				data[i+0] = output[j+2];
-//				data[i+1] = output[j+1];
-//				data[i+2] = output[j+0];
-				data[i+0] = (char)(output[j+2]*(output[j+3]/255.f));
-				data[i+1] = (char)(output[j+1]*(output[j+3]/255.f));
-				data[i+2] = (char)(output[j+0]*(output[j+3]/255.f));
-			}
+			// convert next line
+			format( src, dim.X, dest );
+			src += pitch;
+			jpeg_write_scanlines(&cinfo, row_pointer, 1);
 		}
-		_IRR_ALIGNED_FREE(output);
+
+		delete [] dest;
+
+		/* Step 6: Finish compression */
+		jpeg_finish_compress(&cinfo);
 	}
-	else
-		image = new asset::CImageData(output,nullOffset,imageSize,0,asset::EF_R8G8B8_UNORM,1,true);
 
-	delete [] input;
+	/* Step 7: Destroy */
+	jpeg_destroy_compress(&cinfo);
 
-    images.push_back(image);
-#endif
-
-    asset::ICPUTexture* tex = asset::ICPUTexture::create(images);
-    for (auto img : images)
-        img->drop();
-    return tex;
+	return (dest != 0);
 }
 
-} // end namespace video
-} // end namespace irr
+#endif // _IRR_COMPILE_WITH_LIBJPEG_
 
+CImageWriterJPG::CImageWriterJPG()
+{
+#ifdef _DEBUG
+	setDebugName("CImageWriterJPG");
+#endif
+}
+
+bool CImageWriterJPG::writeAsset(io::IWriteFile* _file, const SAssetWriteParams& _params, IAssetWriterOverride* _override)
+{
+#ifndef _IRR_COMPILE_WITH_LIBJPEG_
+	return false;
+#else
+    if (!_override)
+        getDefaultOverride(_override);
+
+    SAssetWriteContext ctx{_params, _file};
+
+    const asset::CImageData* image =
+#   ifndef _DEBUG
+        static_cast<const asset::CImageData*>(_params.rootAsset);
+#   else
+        dynamic_cast<const asset::CImageData*>(_params.rootAsset);
+#   endif
+    assert(image);
+
+    io::IWriteFile* file = _override->getOutputFile(_file, ctx, {image, 0u});
+    const asset::E_WRITER_FLAGS flags = _override->getAssetWritingFlags(ctx, image, 0u);
+    const float comprLvl = _override->getAssetCompressionLevel(ctx, image, 0u);
+	return writeJPEGFile(file, image, (!!(flags & asset::EWF_COMPRESSED))*(1.f-comprLvl)*100.f); // if quality==0, then it defaults to 75
+#endif
+}
+
+#undef OUTPUT_BUF_SIZE
 #endif
 
